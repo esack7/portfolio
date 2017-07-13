b818e47e25e67a08a1995ede621361d2b6111e70
esack7 <isaacheist@gmail.com>
2017-07-13 11:49:20 -0700

    fixed js files connection issue and mid work on functionality for generating projects

--- js/project.js
+++ js/project.js
@@ -1,11 +1,27 @@
 'use strict';
 
+var projects = [];
+
 //constructor function to instantiate every project in my portfolio
-function Project (name, tools, url, gitHub, colabs, about){
-  this.name = name;
-  this.tools = tools;//tools used in making project
-  this.url = url;
-  this.gitHub = gitHub;
-  this.colabs = colabs;//collaborations
-  this.about = about;
+function Project (projectDataObj){
+  this.name = projectDataObj.name;
+  this.image = projectDataObj.image;
+  this.url = projectDataObj.url;
+  this.gitHubUrl = projectDataObj.gitHubUrl;
+  this.colabs = projectDataObj.colabs;
+  this.description = projectDataObj.description;
+  this.tools = projectDataObj.tools;
 }
+
+Project.prototype.toHtml = function() {
+  var $newProject = $('article.template').clone();
+  $newProject.removeClass('template');
+  $newProject.find('section > h3').html(this.name);
+  $newProject.find('#projectImage').attr('src', this.image);
+  $newProject.find('header > a').attr('href', this.url);
+  $newProject.find('header > a').html(this.gitHubUrl);
+};
+
+projectData.forEach(function(projectObject) {
+  projects.push(new Project(projectObject));
+});
