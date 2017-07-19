c1d3ad25217b8ccf92a1fcb89114bb1a618dcca8
esack7 <isaacheist@gmail.com>
2017-07-19 10:18:26 -0700

    added .json file to replace myProjects.js file's data. json file data gets stored in localStorage.

--- js/project.js
+++ js/project.js
@@ -15,16 +15,46 @@ function Project (projectDataObj){
 
 Project.all = [];
 
+//toHtml is the functionality to utilize handlebars.js
 Project.prototype.toHtml = function() {
   var template = $('#projects-template').html();
   var templateRender = Handlebars.compile(template);
   return templateRender(this);
 };
 
-projectData.forEach(function(projectObject) {
-  Project.all.push(new Project(projectObject));
-});
+//initIndexPage renders objects from Project.all array to the DOM
+Project.initIndexPage = function() {
+  Project.all.forEach(function(project) {
+    $('#projects').append(project.toHtml());
+  });
+}
 
-Project.all.forEach(function(project) {
-  $('#projects').append(project.toHtml());
-});
+//loadAll takes rawData retrieved from localStorage and push's 'new Project' to
+//Project.all array.
+Project.loadAll = function(rawData) {
+  rawData.forEach(function(ele) {
+    Project.all.push(new Project(ele));
+  });
+}
+
+//fetchAll pulls data from projectsData.json, if there is no localStorage and
+//then stores it in localStorage and then renders. If there is localStorage, then
+//it renders without pulling from projectData.json.
+Project.fetchAll = function() {
+  if (localStorage.rawData) {
+    Project.loadAll(JSON.parse(localStorage.rawData));
+    Project.initIndexPage();
+  } else {
+    $.getJSON('data/projectsData.json')
+      .then(
+        function(data) {
+          localStorage.setItem('rawData', JSON.stringify(data));
+          Project.loadAll(data);
+          Project.initIndexPage();
+        },
+        function(error) {
+          console.log('error', error);
+        }
+      )
+  }
+}
