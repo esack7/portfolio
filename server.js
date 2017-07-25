6559f933b5ca13f191f6ee66d6c40268a5f2eb79
esack7 <isaacheist@gmail.com>
2017-07-25 11:57:46 -0700

    did work on portfolio

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 'use strict';
 let express = require('express');
 let app = express();
-const bodyParser =
-  require('body-parser').urlencoded({extended: true});
+// const bodyParser =
+//   require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 app.use(express.static('./public'));
-app.get('*', function(request, response) {
-  response.sendFile('index.html', {root:'.'});
+app.get('*', function(request, response) {response.sendFile('index.html', {root:'.'});
 });
-app.post('/projects', bodyParser, function(request,response) {
-  console.log(request.body);
-  response.send('Record posted to server!!');
-});
+// app.post('/projects', bodyParser, function(request,response) {console.log(request.body);
+//   response.send('Record posted to server!!');
+// });
 
 app.listen(PORT, function() {
   console.log(`Listening on port: "${PORT}"`);
